908e1d0462a3a693cf99b919147283526ef61273
fojo <jonuma9402@gmail.com>
2025-05-24 22:41:56 -0500

    Added oil.nvim

---
+++ lua/custom/plugins/oil.lua
@@ -0,0 +1,28 @@
+return {
+  {
+    'stevearc/oil.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+      require('oil').setup {
+        columns = { 'icon' },
+        keymaps = {
+          ['<C-h>'] = false,
+          ['<M-h>'] = 'actions.select_split',
+        },
+        view_options = {
+          show_hidden = true,
+        },
+      }
+
+      -- Open parent directory in current window
+      vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
+
+      -- Open parent directory in floating window
+      vim.keymap.set('n', '<space>-', require('oil').toggle_float)
+    end,
+  },
+
+  -- NOTE:
+  -- You can preview a file with an oil window open with C-p
+  -- You don't really "close" oil (except for the floating window version, that you close by <space>-, the same way you open), but just go into whatever file you wanted to go into and that "closes" oil.
+}
